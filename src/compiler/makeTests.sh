26d8da474e96b650981bd0a84c8830f0b3be3ecb
Alexey Khokhlov <alexeyxo@iMac.local>
2014-10-15 21:50:13 +0400

    Travis-CI: add compiller tests

--- src/compiler/makeTests.sh
+++ src/compiler/makeTests.sh
@@ -1,4 +1,4 @@
-protoc  --plugin=/usr/local/bin/protoc-gen-objc google/protobuf/unittest* --objc_out="./Tests";
-protoc  --plugin=/usr/local/bin/protoc-gen-objc google/protobuf/descriptor* --objc_out="./Tests";
+protoc google/protobuf/unittest* --objc_out="./Tests";
+#protoc  --plugin=/usr/local/bin/protoc-gen-objc google/protobuf/descriptor* --objc_out="./Tests";
 cp -f ./Tests/Unittest* ../runtime/Tests/;
-cp -f ./Tests/Descriptor* ../runtime/Classes/;
+#cp -f ./Tests/Descriptor* ../runtime/Classes/;
